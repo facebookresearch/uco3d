91e075851918724fef17e6e750d0d2b3deb83221
Roman Shapovalov <romansh@meta.com>
2024-12-19 13:20:48 +0000

    Ordering set lists and adding indices.

--- internal/define_set_lists.py
+++ internal/define_set_lists.py
@@ -1,24 +1,32 @@
+import gc
 import json
+import os
+
 import pandas as pd
 import sqlalchemy as sa
 import sqlite3
 
 # metadata = "/fsx-repligen/shared/datasets/uCO3D/dataset_export/metadata_vgg_1212_166k.sqlite"
-metadata = "/fsx-repligen/shared/datasets/uCO3D/dataset_export/metadata.sqlite"
+# metadata = "/fsx-repligen/shared/datasets/uCO3D/dataset_export/metadata.sqlite"
+metadata = "/fsx-repligen/shared/datasets/uCO3D/dataset_export_tool/temp_database_1218_all/metadata.sqlite"
 # setlists = "/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists_allcat_val1100_dec12.sqlite"
-setlists = "/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_all-categories.sqlite"
+# setlists = "/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_all-categories.sqlite"
+setlists = "/fsx-repligen/shared/datasets/uCO3D/dataset_export_tool/temp_database_1218_all/set_lists_allcat_val1100.sqlite"
+
+output_dir = "/fsx-repligen/shared/datasets/uCO3D/dataset_export_tool/temp_database_1218_all/set_lists"
+
 
 def dump_df_to_sqlite(df, output_path, table_name: str = "set_lists", table_name_length="sequence_lengths"):
     engine = sa.create_engine(f"sqlite:///{output_path}")
 
     res = df.to_sql(table_name, engine, index=False, if_exists="fail")
 
+    seq_order = df["sequence_name"].unique()
     sequence_lengths = (
-        df_setlists
-        .groupby(['sequence_name', 'subset'], as_index=False)
-        .agg(num_frames=('frame_number', 'size'))
-        .sort_values(by=['sequence_name', 'subset'])
-        .reset_index(drop=True)
+        df
+        .groupby(['sequence_name'])
+        .agg(num_frames=('frame_number', 'size'), subset=("subset", "first"))  # NOTE assumes one subset per seq!
+        .reindex(seq_order).reset_index(drop=False)
     )
     if categories is not None:
         sequence_lengths["category"] = sequence_lengths["sequence_name"].map(categories)
@@ -27,8 +35,15 @@ def dump_df_to_sqlite(df, output_path, table_name: str = "set_lists", table_name
 
     with sqlite3.connect(output_path) as conn:
         cursor = conn.cursor()
+        for table in [table_name, table_name_length]:
+            cursor.execute(f"CREATE INDEX idx_{table}_sequence_name ON {table}(sequence_name);")
+            cursor.execute(f"CREATE INDEX idx_{table}_subset ON {table}(subset);")
+        if categories is not None:
+            cursor.execute(f"CREATE INDEX idx_category ON {table_name_length}(category);")
+            cursor.execute(f"CREATE INDEX idx_super_category ON {table_name_length}(super_category);")
         cursor.execute("PRAGMA journal_mode=WAL")
         print(cursor.fetchone())
+
 
 def get_subsets(
     df, threshold=0.3, accurate_threshold=0.2, test_categories=["fireplug", "doughnut", "apple"]
@@ -55,9 +70,8 @@ def get_subsets(
     result["static_train_best"] = static_train_best["sequence_name"].tolist()
     result["static_train"] = df_static.drop(val_random.index.union(val_diverse.index))["sequence_name"].tolist()
 
-    # TODO: sample with threshold, not accurate_threshold for this
     result["debug_train"] = static_train_best[
-        static_train_best["category"].isin(test_categories)
+        (static_train_best["score"] > threshold) & static_train_best["category"].isin(test_categories)
     ].groupby('category').sample(n=5, random_state=42)["sequence_name"].tolist()
     val = pd.concat([val_random, val_diverse]).drop_duplicates()
     result["debug_val"] = val[val["category"].isin(test_categories)]["sequence_name"].tolist()
@@ -75,7 +89,9 @@ def dump_subset(setlists, train_sequences, val_sequences):
 
     return setlists_res
 
+
 if __name__ == "__main__":
+    assert os.path.isfile(setlists)
     with sqlite3.connect(setlists) as conn:
         # SQL query to select all data from the table 'sequence_annots'
         query = "SELECT * FROM set_lists"
@@ -104,27 +120,37 @@ if __name__ == "__main__":
     print({k: len(v) for k, v in result.items()})
 
     alex_setlists_trainval = alex_setlists[alex_setlists["subset"] != "test"]
+    alex_setlists_trainval = alex_setlists_trainval.sort_values(
+        by="sequence_name",
+        key=lambda x: x.map(scores),
+        ascending=False,
+        kind="mergesort",
+    ).reset_index(drop=True)
+
+    gc.collect()
 
     print("Subsets done. Dumping")
 
+    os.makedirs(output_dir, exist_ok=True)
+
     df_setlists = dump_subset(alex_setlists_trainval, result["debug_train"], result["debug_val"])
-    dump_df_to_sqlite(df_setlists, f"/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_3categories-debug.sqlite")
+    dump_df_to_sqlite(df_setlists, f"{output_dir}/set_lists_3categories-debug.sqlite")
 
     # df_setlists = dump_subset(alex_setlists_trainval, result["static_train"], result["static_val_diverse"])
     # dump_df_to_sqlite(df_setlists, f"/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_static_diverse_val.sqlite")
 
     df_setlists = dump_subset(alex_setlists_trainval, result["static_train"], result["static_val_diverse"] + result["static_val_random"])
-    dump_df_to_sqlite(df_setlists, f"/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_static-categories.sqlite")
+    dump_df_to_sqlite(df_setlists, f"{output_dir}/set_lists_static-categories.sqlite")
 
     df_setlists = dump_subset(alex_setlists_trainval, result["dynamic_train"], result["dynamic_val"])
-    dump_df_to_sqlite(df_setlists, f"/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_dynamic-categories.sqlite")
+    dump_df_to_sqlite(df_setlists, f"{output_dir}/set_lists_dynamic-categories.sqlite")
 
     df_setlists = dump_subset(
         alex_setlists_trainval, result["static_train"] + result["dynamic_train"], result["static_val_diverse"] + result["static_val_random"] + result["dynamic_val"]
     )
-    dump_df_to_sqlite(df_setlists, f"/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_all-categories.sqlite")
+    dump_df_to_sqlite(df_setlists, f"{output_dir}/set_lists_all-categories.sqlite")
 
     df_setlists = dump_subset(
         alex_setlists_trainval, result["static_train_best"], result["static_val_diverse"] + result["static_val_random"]
     )
-    dump_df_to_sqlite(df_setlists, f"/fsx-repligen/shared/datasets/uCO3D/dataset_export/set_lists/set_lists_static-categories-accurate-reconstruction.sqlite")
+    dump_df_to_sqlite(df_setlists, f"{output_dir}/set_lists_static-categories-accurate-reconstruction.sqlite")
